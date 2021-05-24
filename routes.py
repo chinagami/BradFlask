e045a3f03abd01abb054a05a8d6a1bfd38cf3c94
William Vuong <aznguyy@hotmail.com>
2021-05-23 21:04:01 -0400

    user profile

--- routes.py
+++ routes.py
@@ -57,3 +57,14 @@ def register():
         flash('You are now registered.')
         return redirect(url_for('login'))
     return render_template('register.html', form=form)
+
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+    {'author': user, 'body': 'Test post #1'},
+    {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', posts=posts, user=user)
